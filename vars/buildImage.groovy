dc9d0cad61e6cc33e664b479dc64d1719fc7b87b
flaviassantos <ssantosflavia@hotmail.com>
2023-04-24 15:52:22 +0200

    fix buildImage logic

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,10 +1,10 @@
 #!/usr/bin/env groovy
 
 def call() {
-    echo "building the docker image..."
-    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t nanajanashia/demo-app:jma-2.0 .'
+    echo "Building the docker image..."
+    withCredentials([usernamePassword(credentialsId: 'docker-hub-credentials', passwordVariable:'PASS', usernameVariable: 'USER')]){
+        sh "docker build -t flaviassantos/my-repo:jma-2.0 ."
         sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh 'docker push nanajanashia/demo-app:jma-2.0'
+        sh "docker push flaviassantos/my-repo:jma-2.0"
     }
 }
