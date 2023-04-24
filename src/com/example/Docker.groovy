9dc70f4711c087b1c20729e8dff73cdde1320786
flaviassantos <ssantosflavia@hotmail.com>
2023-04-24 20:26:56 +0200

    fixed dockerLogin

--- src/com/example/Docker.groovy
+++ src/com/example/Docker.groovy
@@ -15,7 +15,7 @@ class Docker implements Serializable {
     }
 
     def dockerLogin() {
-        script.withCredentials([script.usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+        script.withCredentials([script.usernamePassword(credentialsId: 'docker-hub-credentials', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
             script.sh "echo $script.PASS | docker login -u $script.USER --password-stdin"
         }
     }
