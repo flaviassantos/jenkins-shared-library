0873aa93795d2c783d64f4634a983196df158930
flaviassantos <ssantosflavia@hotmail.com>
2023-04-24 20:16:11 +0200

    split buildDockerImage with separated funtions

--- src/com/example/Docker.groovy
+++ src/com/example/Docker.groovy
@@ -10,13 +10,18 @@ class Docker implements Serializable {
     }
 
     def buildDockerImage(String imageName) {
-        script.echo "Building the docker image..."
-        script.withCredentials([script.usernamePassword(credentialsId: 'docker-hub-credentials', passwordVariable:'PASS', usernameVariable: 'USER')]){
-            script.sh "docker build -t $imageName ."
+        script.echo "building the docker image..."
+        script.sh "docker build -t $imageName ."
+    }
+
+    def dockerLogin() {
+        script.withCredentials([script.usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
             script.sh "echo $script.PASS | docker login -u $script.USER --password-stdin"
-            script.sh "docker push $imageName"
         }
     }
 
-}
+    def dockerPush(String imageName) {
+        script.sh "docker push $imageName"
+    }
 
+}
